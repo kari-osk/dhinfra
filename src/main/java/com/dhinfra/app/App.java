c50ef10d9513d89a9d715b89c1ffa99e5050f004
kari-osk <karinaosuka@gmail.com>
2022-04-06 11:49:59 -0300

    atualizar app

--- src/main/java/com/dhinfra/app/App.java
+++ src/main/java/com/dhinfra/app/App.java
@@ -4,10 +4,8 @@ package com.dhinfra.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World Aula de infra");
     }
 }
