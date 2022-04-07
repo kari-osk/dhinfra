4a01ce9cba8079d59851cba8b494cdbc8bc36fcf
kari-osk <karinaosuka@gmail.com>
2022-04-07 11:50:51 -0300

    teste error

--- src/main/java/com/dhinfra/app/App.java
+++ src/main/java/com/dhinfra/app/App.java
@@ -1,11 +1,22 @@
 package com.dhinfra.app;
 
+import static org.junit.Assert.assertTrue;
+
+import java.beans.Transient;
+
+import org.junit.Test;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World Aula de infra");
+    // public static void main(String[] args) {
+    // System.out.println("Hello World Aula de infra");
+    // }
+    // }
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(false);
     }
 }
